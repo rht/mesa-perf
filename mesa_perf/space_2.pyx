1c3c7d7cf3690f7e9ea64f5473ee7c1efdecf9a2
Tortar <68152031+Tortar@users.noreply.github.com>
2023-01-20 01:11:46 +0100

    Create space_2.pyx

---
+++ mesa_perf/space_2.pyx
@@ -0,0 +1,120 @@
+# cython: infer_types=True, language_level=3
+
+cimport cython
+import numpy as np
+
+
+cdef class _Grid:
+    cdef long height, width, num_cells
+    cdef bint torus
+    cdef object[:, :] _grid
+    cdef dict _agent_map
+
+    def __init__(self, long width, long height, bint torus):
+        self.height = height
+        self.width = width
+        self.torus = torus
+        self.num_cells = height * width
+
+        self._grid = np.full((self.width, self.height), self.default_val(), dtype=object)
+
+    cpdef default_val(self):
+        return None
+
+    cpdef is_cell_empty(self, pos):
+        cdef long x, y
+
+        x, y = pos[0], pos[1]
+        return self._grid[x, y] == self.default_val()
+
+    cpdef place_agent(self, agent, pos):
+        cdef long x, y
+
+        if self.is_cell_empty(pos):
+            x, y = pos[0], pos[1]
+            agent_id = agent.unique_id
+            self._grid[x, y] = agent
+            agent.pos = pos
+        else:
+            raise Exception("Cell not empty")
+
+    @cython.wraparound(False)
+    @cython.boundscheck(False)
+    cpdef list get_cell_list_contents(self, object cell_list):
+        cdef long x, y
+        cdef int count
+        cdef long[:] ids_mview
+
+        length = len(cell_list)
+        agent_mview = np.ndarray(length, object)
+
+        count = 0
+        default_val = self.default_val()
+        for i in range(length):
+            pos = cell_list[i]
+            x, y = pos[0], pos[1]
+            agent = self._grid[x, y]
+            if agent == default_val:
+                continue
+            agent_mview[count] = agent
+            count += 1
+        
+        return [agent for k in agent_mview[:count]]
+
+    @cython.wraparound(False)
+    @cython.boundscheck(False)
+    cpdef list get_neighborhood(self, object pos, bint moore, int radius, bint include_center):
+
+        cdef long[:, :] neighborhood
+        cdef long nx, ny
+        cdef int x_radius, y_radius, dx, dy, kx, ky
+        cdef int min_x_range, max_x_range, min_y_range, max_y_range
+        cdef int x, y, count
+        
+        neighborhood = np.empty(((radius*2+1)**2, 2), int)
+        x, y = pos[0], pos[1]
+        count = 0
+        if self.torus:
+
+            x_max_radius, y_max_radius = self.width // 2, self.height // 2
+            x_radius, y_radius = min(radius, x_max_radius), min(radius, y_max_radius)
+
+            xdim_even, ydim_even = (self.width + 1) % 2, (self.height + 1) % 2
+            kx = 1 if x_radius == x_max_radius and xdim_even else 0
+            ky = 1 if y_radius == y_max_radius and ydim_even else 0
+
+            for dx in range(-x_radius, x_radius + 1 - kx):
+                for dy in range(-y_radius, y_radius + 1 - ky):
+
+                    if not moore and abs(dx) + abs(dy) > radius:
+                        continue
+
+                    nx = (x + dx) % self.width
+                    ny = (y + dy) % self.height
+
+                    if nx == x and ny == y and not include_center:
+                        continue
+                    
+                    neighborhood[count, 0] = nx
+                    neighborhood[count, 1] = ny
+                    count += 1
+        else:
+            min_x_range = max(0, x - radius)
+            max_x_range = min(self.width, x + radius + 1)
+            min_y_range = max(0, y - radius)
+            max_y_range = min(self.height, y + radius + 1)
+
+            for nx in range(min_x_range, max_x_range):
+                for ny in range(min_y_range, max_y_range):
+
+                    if not moore and abs(nx - x) + abs(ny - y) > radius:
+                        continue
+
+                    if nx == x and ny == y and not include_center:
+                        continue
+                    
+                    neighborhood[count, 0] = nx
+                    neighborhood[count, 1] = ny
+                    count += 1
+
+        return [(neighborhood[i, 0], neighborhood[i, 1]) for i in range(count)]
