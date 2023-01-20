0256e51819ccb3c6cfc2d29fdc88028d52a1498a
Tortar <68152031+Tortar@users.noreply.github.com>
2023-01-20 17:25:12 +0100

    add grid __init__ benchmark

--- mesa_perf/benchmark.py
+++ mesa_perf/benchmark.py
@@ -15,7 +15,7 @@ random.seed(1)
 width = 100
 height = 100
 grid = mesa.space.SingleGrid(width, height, False)
-for i in range(0):
+for i in range(10000):
     agent = mesa.Agent(i, None)
     while True:
         x = random.randrange(width)
@@ -25,6 +25,10 @@ for i in range(0):
     grid.place_agent(agent, (x, y))
 cell_list = grid.get_neighborhood((10, 10), True, include_center=True, radius=10)
 """
+
+stmt = "mesa.space.SingleGrid(width, height, False)"
+print_elapsed("python grid __init__", setup, stmt)
+
 stmt = "grid._neighborhood_cache = {}; grid.get_neighborhood((10, 10), True, include_center=True, radius=10)"
 print_elapsed("python get_neighborhood", setup, stmt)
 
@@ -39,7 +43,7 @@ random.seed(1)
 width = 100
 height = 100
 grid = {0}(width, height, False)
-for i in range(0):
+for i in range(10000):
     agent = mesa.Agent(i, None)
     while True:
         x = random.randrange(width)
@@ -50,11 +54,17 @@ for i in range(0):
 cell_list = grid.get_neighborhood((10, 10), True, include_center=True, radius=10)
 """
 
+stmt = "_Grid(width, height, False)"
+print_elapsed("cython with map grid __init__", setup.format("_Grid"), stmt)
+
 stmt = "grid.get_neighborhood((10, 10), True, include_center=True, radius=10)"
 print_elapsed("cython with map get_neighborhood", setup.format("_Grid"), stmt)
 
 stmt = "grid.get_cell_list_contents(cell_list)"
 print_elapsed("cython with map get_cell_list_contents", setup.format("_Grid"), stmt)
+
+stmt = "_Grid_NoMap(width, height, False)"
+print_elapsed("cython grid __init__", setup.format("_Grid_NoMap"), stmt)
 
 stmt = "grid.get_neighborhood((10, 10), True, include_center=True, radius=10)"
 print_elapsed("cython get_neighborhood", setup.format("_Grid_NoMap"), stmt)
