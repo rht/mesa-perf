a097c11fa66b086a832472f5253a18932d4abd9e
rht <rhtbot@protonmail.com>
2023-01-21 07:44:29 -0500

    scheduler benchmark: Add shuffling

--- experiments/scheduler/benchmark.py
+++ experiments/scheduler/benchmark.py
@@ -13,33 +13,65 @@ def print_elapsed(label, setup, stmt):
 setup = """
 import {0}
 model = {0}.Model()
-schedule = {1}(model)
+schedule = {1}(model{3})
 for i in range(200):
     agent = {2}(i, model)
     schedule.add(agent)
 """
 stmt = "schedule.step()"
 print_elapsed(
-    "default", setup.format("mesa", "mesa.time.RandomActivation", "mesa.Agent"), stmt
+    "default", setup.format("mesa", "mesa.time.BaseScheduler", "mesa.Agent", ""), stmt
 )
-
 print_elapsed(
-    "numba typed dict",
-    setup.format("numba_version", "numba_version.Scheduler", "numba_version.Agent"),
+    "default shuffled",
+    setup.format("mesa", "mesa.time.RandomActivation", "mesa.Agent", ""),
     stmt,
 )
+
+
+# print_elapsed(
+#     "numba typed dict",
+#     setup.format("numba_version", "numba_version.Scheduler", "numba_version.Agent"),
+#     stmt,
+# )
 
 print_elapsed(
     "cython python dict",
     "import cython_version\n"
-    + setup.format("mesa", "cython_version.SchedulerPythonDict", "mesa.Agent"),
+    + setup.format(
+        "mesa", "cython_version.SchedulerPythonDict", "mesa.Agent", ", False"
+    ),
     stmt,
 )
+print_elapsed(
+    "cython python dict shuffled",
+    "import cython_version\n"
+    + setup.format(
+        "mesa", "cython_version.SchedulerPythonDict", "mesa.Agent", ", True"
+    ),
+    stmt,
+)
+
+
 print_elapsed(
     "cython python dict, cython agent",
     "import cython_version\n"
     + setup.format(
-        "mesa", "cython_version.SchedulerPythonDict", "cython_version.Agent"
+        "mesa",
+        "cython_version.SchedulerDictCythonizedAgent",
+        "cython_version.Agent",
+        ", False",
+    ),
+    stmt,
+)
+print_elapsed(
+    "cython python dict, cython agent shuffled",
+    "import cython_version\n"
+    + setup.format(
+        "mesa",
+        "cython_version.SchedulerDictCythonizedAgent",
+        "cython_version.Agent",
+        ", True",
     ),
     stmt,
 )
--- experiments/scheduler/cython_version.pyx
+++ experiments/scheduler/cython_version.pyx
@@ -1,20 +1,27 @@
 # cython: infer_types=True, language_level=3
 cimport cython
 # from libcpp.map cimport map as cpp_map
+import numpy as np
 
 cdef class SchedulerPythonDict:
     cdef object model
     cdef dict _agents
-    def __init__(self, object model):
+    cdef bint shuffle
+    def __init__(self, object model, bint shuffle):
         self.model = model
         self._agents = {}
+        self.shuffle = shuffle
 
     cpdef add(self, object agent):
         self._agents[agent.unique_id] = agent
 
-    cpdef step(self):
-        for agent in self._agents.values():
-            agent.step()
+    def step(self):
+        agent_keys = list(self._agents.keys())
+        if self.shuffle:
+            #for agent_key in agent_keys[np.random.permutation(len(agent_keys))]:
+            self.model.random.shuffle(agent_keys)
+        for agent_key in agent_keys:
+            self._agents[agent_key].step()
 
 
 cdef class Agent:
@@ -31,16 +38,24 @@ cdef class Agent:
 cdef class SchedulerDictCythonizedAgent:
     cdef object model
     cdef dict _agents
-    def __init__(self, object model):
+    cdef bint shuffle
+    def __init__(self, object model, bint shuffle):
         self.model = model
         self._agents = {}
+        self.shuffle = shuffle
 
     cpdef add(self, Agent agent):
         self._agents[agent.unique_id] = agent
 
     cpdef step(self):
         cdef Agent agent
-        for agent in self._agents.values():
+        cdef list agent_keys
+        cdef long agent_key
+        agent_keys = list(self._agents.keys())
+        if self.shuffle:
+            self.model.random.shuffle(agent_keys)
+        for agent_key in agent_keys:
+            agent = self._agents[agent_key]
             agent.step()
 
 
