091536b4fc4a398f8c81f40675d88d2a3b31e3e3
Tortar <68152031+Tortar@users.noreply.github.com>
2023-01-24 01:03:15 +0100

    benchmarks with memoryviews added

--- mesa_perf/benchmark.py
+++ mesa_perf/benchmark.py
@@ -1,6 +1,6 @@
 import timeit
 
-repetition = 1000
+repetition = 2000
 
 def print_elapsed(label, setup, stmt):
     _elapsed = timeit.timeit(stmt, setup, number=repetition) * 10**6 / repetition
@@ -12,8 +12,8 @@ setup = """
 import mesa
 import random
 random.seed(1)
-width = 100
-height = 100
+width = 10
+height = 10
 grid = mesa.space.SingleGrid(width, height, False)
 for i in range(0):
     agent = mesa.Agent(i, None)
@@ -23,7 +23,7 @@ for i in range(0):
         if grid.is_cell_empty((x, y)):
             break
     grid.place_agent(agent, (x, y))
-cell_list = grid.get_neighborhood((10, 10), True, include_center=True, radius=10)
+cell_list = grid.get_neighborhood((5, 5), True, include_center=True, radius=1)
 """
 
 print("\ndefault\n")
@@ -32,7 +32,7 @@ stmt = "mesa.space.SingleGrid(width, height, False)"
 elapsed_init_default = print_elapsed("python grid __init__", setup, stmt)
 print()
 
-stmt = "grid._neighborhood_cache = {}; grid.get_neighborhood((10, 10), True, include_center=True, radius=10)"
+stmt = "grid._neighborhood_cache = {}; grid.get_neighborhood((10, 10), True, include_center=True, radius=1)"
 elapsed_neighborhood_default = print_elapsed("python get_neighborhood", setup, stmt)
 print()
 
@@ -45,8 +45,8 @@ import mesa
 from space import {0}
 import random
 random.seed(1)
-width = 100
-height = 100
+width = 10
+height = 10
 grid = {0}(width, height, False)
 for i in range(0):
     agent = mesa.Agent(i, None)
@@ -56,7 +56,9 @@ for i in range(0):
         if grid.is_cell_empty((x, y)):
             break
     grid.place_agent(agent, (x, y))
-cell_list = grid.get_neighborhood((10, 10), True, include_center=True, radius=10)
+cell_list = grid.get_neighborhood((10, 10), True, include_center=True, radius=1)
+#print(cell_list)
+cell_view = grid.convert_tuples_to_mview(cell_list)
 """
 
 print("\ntimings with the map\n")
@@ -65,13 +67,22 @@ stmt = "_Grid(width, height, False)"
 elapsed_init_cython = print_elapsed("cython with map grid __init__", setup.format("_Grid"), stmt)
 print(" --> speedup", round(elapsed_init_default / elapsed_init_cython, 2))
 
-stmt = "grid.get_neighborhood((10, 10), True, include_center=True, radius=10)"
-elapsed_neighborhood_cython = print_elapsed("cython with map get_neighborhood", setup.format("_Grid"), stmt)
-print(" --> speedup", round(elapsed_neighborhood_default / elapsed_neighborhood_cython, 2))
+stmt = "grid.get_neighborhood((10, 10), True, include_center=True, radius=1)"
+elapsed_neighborhood_nomap = print_elapsed("cython no map get_neighborhood", setup.format("_Grid"), stmt)
+print(" --> speedup", round(elapsed_neighborhood_default / elapsed_neighborhood_nomap, 2))
 
 stmt = "grid.get_cell_list_contents(cell_list)"
-elapsed_cl_cython = print_elapsed("cython with map get_cell_list_contents", setup.format("_Grid"), stmt)
-print(" --> speedup", round(elapsed_cl_default / elapsed_cl_cython, 2))
+elapsed_cl_nomap = print_elapsed("cython no map get_cell_list_contents", setup.format("_Grid"), stmt)
+print(" --> speedup", round(elapsed_cl_default / elapsed_cl_nomap, 2))
+
+stmt = "grid.get_neighborhood_mview((10, 10), True, include_center=True, radius=1)"
+elapsed_neighborhood_nomap = print_elapsed("cython no map get_neighborhood_mview", setup.format("_Grid"), stmt)
+print(" --> speedup", round(elapsed_neighborhood_default / elapsed_neighborhood_nomap, 2))
+
+stmt = "grid.get_cell_mview_contents(cell_view)"
+elapsed_cl_nomap = print_elapsed("cython no map get_cell_mview_contents", setup.format("_Grid"), stmt)
+print(" --> speedup", round(elapsed_cl_default / elapsed_cl_nomap, 2))
+
 
 print("\ntimings without the map\n")
 
@@ -79,10 +90,18 @@ stmt = "_Grid_NoMap(width, height, False)"
 elapsed_init_nomap = print_elapsed("cython no map grid __init__", setup.format("_Grid_NoMap"), stmt)
 print(" --> speedup", round(elapsed_init_default / elapsed_init_nomap, 2))
 
-stmt = "grid.get_neighborhood((10, 10), True, include_center=True, radius=10)"
+stmt = "grid.get_neighborhood((10, 10), True, include_center=True, radius=1)"
 elapsed_neighborhood_nomap = print_elapsed("cython no map get_neighborhood", setup.format("_Grid_NoMap"), stmt)
 print(" --> speedup", round(elapsed_neighborhood_default / elapsed_neighborhood_nomap, 2))
 
 stmt = "grid.get_cell_list_contents(cell_list)"
 elapsed_cl_nomap = print_elapsed("cython no map get_cell_list_contents", setup.format("_Grid_NoMap"), stmt)
+print(" --> speedup", round(elapsed_cl_default / elapsed_cl_nomap, 2))
+
+stmt = "grid.get_neighborhood_mview((10, 10), True, include_center=True, radius=1)"
+elapsed_neighborhood_nomap = print_elapsed("cython no map get_neighborhood_mview", setup.format("_Grid_NoMap"), stmt)
+print(" --> speedup", round(elapsed_neighborhood_default / elapsed_neighborhood_nomap, 2))
+
+stmt = "grid.get_cell_mview_contents(cell_view)"
+elapsed_cl_nomap = print_elapsed("cython no map get_cell_mview_contents", setup.format("_Grid_NoMap"), stmt)
 print(" --> speedup", round(elapsed_cl_default / elapsed_cl_nomap, 2))
