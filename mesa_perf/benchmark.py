176b05a267d83cd8800cd7e1619d2ccfc0ea15c0
Tortar <68152031+Tortar@users.noreply.github.com>
2023-01-31 16:31:22 +0100

    more benchmarks

--- mesa_perf/benchmark.py
+++ mesa_perf/benchmark.py
@@ -1,7 +1,7 @@
 import timeit
 
 repetition = 1000
-radius = 10
+radius = 1
 density = 0.5
 
 def print_elapsed(label, setup, stmt):
@@ -10,17 +10,18 @@ def print_elapsed(label, setup, stmt):
     print(label, elapsed, end="")
     return _elapsed
 
-setup = """
-import mesa
+main_setup = """
+from {0} import SingleGrid
+from mesa import Agent, Model
 import random
 random.seed(1)
-density = {0}
-width = 200
-height = 200
-grid = mesa.space.SingleGrid(width, height, False)
-model = mesa.Model()
+density = {1}
+width = 100
+height = 100
+grid = SingleGrid(width, height, True)
+model = Model()
 for i in range(round(density*width*height)):
-    agent = mesa.Agent(i, model)
+    agent = Agent(i, model)
     while True:
         x = random.randrange(width)
         y = random.randrange(height)
@@ -28,14 +29,17 @@ for i in range(round(density*width*height)):
             break
     grid.place_agent(agent, (x, y))
 pos = (x,y)
-cell_list = grid.get_neighborhood(pos, True, include_center=False, radius={1})
-""".format(density, radius)
+pos_2 = (-3, -5)
+cell_list = grid.get_neighborhood(pos, True, include_center=True, radius={2})
+"""
+
+setup = main_setup.format("mesa.space", density, radius)
 
 print("\ntimings default\n")
 
 descr = "python grid "
 
-stmt = "mesa.space.SingleGrid(width, height, False)"
+stmt = "SingleGrid(width, height, False)"
 method = "__init__"
 elapsed_init_default = print_elapsed(descr +  method, setup, stmt)
 print()
@@ -70,11 +74,127 @@ method = "move_to_empty"
 elapsed_move_to_empty_default = print_elapsed(descr +  method, setup, stmt)
 print()
 
+stmt = "grid.remove_agent(agent)"
+method = "remove_agent"
+elapsed_remove_agent_default = print_elapsed(descr +  method, setup, stmt)
+print()
+
+stmt = "grid.remove_agent(agent); grid.place_agent(agent, pos)"
+method = "place_agent"
+elapsed_place_agent_default = print_elapsed(descr +  method, setup, stmt)
+print()
+
+stmt = "grid.torus_adj(pos_2)"
+method = "torus_adj"
+elapsed_torus_adj_default = print_elapsed(descr +  method, setup, stmt)
+print()
+
+stmt = "grid.empties"
+method = "build and call empties"
+elapsed_empties_default = print_elapsed(descr +  method, setup, stmt)
+print()
+
 stmt = "for x in grid.iter_cell_list_contents(cell_list): x"
 method = "iter_cell_list_contents"
 elapsed_iter_cell_list_contents_default = print_elapsed(descr +  method, setup, stmt)
 print()
 
+stmt = "for x in grid.iter_neighbors(pos, True, include_center=False, radius={}): x".format(radius)
+method = "iter_neighbors"
+elapsed_iter_neighbors_default = print_elapsed(descr +  method, setup, stmt)
+print()
+
+stmt = "for x in grid.coord_iter(): x"
+method = "coord_iter"
+elapsed_coord_iter_default = print_elapsed(descr +  method, setup, stmt)
+print()
+
+stmt = "for x in grid: x"
+method = "__iter__"
+elapsed___iter___default = print_elapsed(descr +  method, setup, stmt)
+print()
+
+
+setup = main_setup.format("space", density, radius)
+
+descr = "cython grid "
+
+print("\ntimings cython\n")
+
+stmt = "SingleGrid(width, height, False)"
+method = "__init__"
+elapsed_init_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_init_default / elapsed_init_cython, 2))
+
+stmt = "grid.get_neighborhood(pos, True, include_center=False, radius={})".format(radius)
+method = "get_neighborhood"
+elapsed_neighborhood_nomap = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_get_neighborhood_default / elapsed_neighborhood_nomap, 2))
+
+stmt = "grid.get_cell_list_contents(cell_list)"
+method = "get_cell_list_contents"
+elapsed_cl_nomap = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_get_cell_list_contents_default / elapsed_cl_nomap, 2))
+
+stmt = "grid.get_neighbors(pos, True, include_center=False, radius={})".format(radius)
+method = "get_neighbors"
+elapsed_neighbors_nomap = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_get_neighbors_default / elapsed_neighbors_nomap, 2))
+
+stmt = "grid.out_of_bounds(pos)"
+method = "out_of_bounds"
+elapsed_out_of_bounds_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_out_of_bounds_default / elapsed_out_of_bounds_cython, 2))
+
+stmt = "grid.is_cell_empty(pos)"
+method = "is_cell_empty"
+elapsed_is_cell_empty_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_is_cell_empty_default / elapsed_is_cell_empty_cython, 2))
+
+stmt = "grid.move_to_empty(agent)"
+method = "move_to_empty"
+elapsed_move_to_empty_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_move_to_empty_default / elapsed_move_to_empty_cython, 2))
+
+stmt = "grid.remove_agent(agent)"
+method = "remove_agent"
+elapsed_remove_agent_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_remove_agent_default / elapsed_remove_agent_cython, 2))
+
+stmt = "grid.remove_agent(agent); grid.place_agent(agent, pos)"
+method = "place_agent"
+elapsed_place_agent_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round((elapsed_place_agent_default-elapsed_remove_agent_default)/ (elapsed_place_agent_cython-elapsed_remove_agent_cython), 2))
+
+stmt = "grid.torus_adj(pos_2)"
+method = "torus_adj"
+elapsed_torus_adj_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_torus_adj_default / elapsed_torus_adj_cython, 2))
+
+stmt = "grid.empties"
+method = "build and call empties"
+elapsed_empties_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_empties_default / elapsed_empties_cython, 2))
+
+stmt = "for x in grid.iter_cell_list_contents(cell_list): x"
+method = "iter_cell_list_contents"
+elapsed_cl_nomap = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_iter_cell_list_contents_default / elapsed_cl_nomap, 2))
+
+stmt = "for x in grid.iter_neighbors(pos, True, include_center=False, radius={}): x".format(radius)
+method = "iter_neighbors"
+elapsed_iter_neighbors_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_iter_neighbors_default / elapsed_iter_neighbors_cython, 2))
+
+stmt = "for x in grid.coord_iter(): x"
+method = "coord_iter"
+elapsed_coord_iter_cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed_coord_iter_default / elapsed_coord_iter_cython, 2))
+
+stmt = "for x in grid: x"
+method = "__iter__"
+elapsed___iter___cython = print_elapsed(descr +  method, setup, stmt)
+print(" --> speedup", round(elapsed___iter___default / elapsed___iter___cython, 2))
 
 setup = """
 import mesa
@@ -136,70 +256,3 @@ stmt = "grid.get_neighbors((10, 10), True, include_center=True, radius={})".form
 method = "get_neighbors"
 elapsed_neighbors_nomap = print_elapsed("cython with map get_neighbors", setup.format("_Grid"), stmt)
 print(" --> speedup", round(elapsed_neighbors_default / elapsed_neighbors_nomap, 2))
-
-setup = """
-import mesa
-from space import {0}
-import random
-random.seed(1)
-density = {1}
-width = 200
-height = 200
-grid = {0}(width, height, False)
-model = mesa.Model()
-for i in range(round(density*width*height)):
-    agent = mesa.Agent(i, model)
-    while True:
-        x = random.randrange(width)
-        y = random.randrange(height)
-        if grid.is_cell_empty((x, y)):
-            break
-    grid.place_agent(agent, (x, y))
-pos = (x,y)
-cell_list = grid.get_neighborhood(pos, True, include_center=True, radius={2})
-""".format("_BaseSingleGrid", density, radius)
-
-descr = "cython grid "
-
-print("\ntimings cython\n")
-
-stmt = "_BaseSingleGrid(width, height, False)"
-method = "__init__"
-elapsed_init_cython = print_elapsed(descr +  method, setup, stmt)
-print(" --> speedup", round(elapsed_init_default / elapsed_init_cython, 2))
-
-stmt = "grid.get_neighborhood(pos, True, include_center=False, radius={})".format(radius)
-method = "get_neighborhood"
-elapsed_neighborhood_nomap = print_elapsed(descr +  method, setup, stmt)
-print(" --> speedup", round(elapsed_get_neighborhood_default / elapsed_neighborhood_nomap, 2))
-
-stmt = "grid.get_cell_list_contents(cell_list)"
-method = "get_cell_list_contents"
-elapsed_cl_nomap = print_elapsed(descr +  method, setup, stmt)
-print(" --> speedup", round(elapsed_get_cell_list_contents_default / elapsed_cl_nomap, 2))
-
-stmt = "grid.get_neighbors(pos, True, include_center=False, radius={})".format(radius)
-method = "get_neighbors"
-elapsed_neighbors_nomap = print_elapsed(descr +  method, setup, stmt)
-print(" --> speedup", round(elapsed_get_neighbors_default / elapsed_neighbors_nomap, 2))
-
-stmt = "grid.out_of_bounds(pos)"
-method = "out_of_bounds"
-elapsed_out_of_bounds_cython = print_elapsed(descr +  method, setup, stmt)
-print(" --> speedup", round(elapsed_out_of_bounds_default / elapsed_out_of_bounds_cython, 2))
-
-stmt = "grid.is_cell_empty(pos)"
-method = "is_cell_empty"
-elapsed_is_cell_empty_cython = print_elapsed(descr +  method, setup, stmt)
-print(" --> speedup", round(elapsed_is_cell_empty_default / elapsed_is_cell_empty_cython, 2))
-
-stmt = "grid.move_to_empty(agent)"
-method = "move_to_empty"
-elapsed_move_to_empty_cython = print_elapsed(descr +  method, setup, stmt)
-print(" --> speedup", round(elapsed_move_to_empty_default / elapsed_move_to_empty_cython, 2))
-
-stmt = "for x in grid.iter_cell_list_contents(cell_list): x"
-method = "iter_cell_list_contents"
-elapsed_cl_nomap = print_elapsed(descr +  method, setup, stmt)
-print(" --> speedup", round(elapsed_iter_cell_list_contents_default / elapsed_cl_nomap, 2))
-
