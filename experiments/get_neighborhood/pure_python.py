5e17927eb82cf18f5e9d5cfee6e427fc6f3aecab
rht <rhtbot@protonmail.com>
2024-02-17 20:52:38 -0500

    Update pure_python.py to be the same as Mesa main

--- experiments/get_neighborhood/pure_python.py
+++ experiments/get_neighborhood/pure_python.py
@@ -1,3 +1,10 @@
+def out_of_bounds(pos, width, height) -> bool:
+    """Determines whether position is off the grid, returns the out of
+    bounds coordinate."""
+    x, y = pos
+    return x < 0 or x >= width or y < 0 or y >= height
+
+
 def get_neighborhood(pos, moore, include_center, radius, torus, width, height):
     """Return a list of cells that are in the neighborhood of a
     certain point.
@@ -22,48 +29,48 @@ def get_neighborhood(pos, moore, include_center, radius, torus, width, height):
     # if neighborhood is not None:
     #    return neighborhood
 
-    # We use a list instead of a dict for the neighborhood because it would
-    # be easier to port the code to Cython or Numba (for performance
-    # purpose), with minimal changes. To better understand how the
-    # algorithm was conceived, look at
-    # https://github.com/projectmesa/mesa/pull/1476#issuecomment-1306220403
-    # and the discussion in that PR in general.
-    neighborhood = []
+    # we use a dict to keep insertion order
+    neighborhood = {}
 
     x, y = pos
-    if torus:
-        x_max_radius, y_max_radius = self.width // 2, self.height // 2
-        x_radius, y_radius = min(radius, x_max_radius), min(radius, y_max_radius)
 
-        # For each dimension, in the edge case where the radius is as big as
-        # possible and the dimension is even, we need to shrink by one the range
-        # of values, to avoid duplicates in neighborhood. For example, if
-        # the width is 4, while x, x_radius, and x_max_radius are 2, then
-        # (x + dx) has a value from 0 to 4 (inclusive), but this means that
-        # the 0 position is repeated since 0 % 4 and 4 % 4 are both 0.
-        xdim_even, ydim_even = (self.width + 1) % 2, (self.height + 1) % 2
-        kx = int(x_radius == x_max_radius and xdim_even)
-        ky = int(y_radius == y_max_radius and ydim_even)
+    # First we check if the neighborhood is inside the grid
+    if (
+        x >= radius
+        and width - x > radius
+        and y >= radius
+        and height - y > radius
+    ):
+        # If the radius is smaller than the distance from the borders, we
+        # can skip boundary checks.
+        x_range = range(x - radius, x + radius + 1)
+        y_range = range(y - radius, y + radius + 1)
 
-        for dx in range(-x_radius, x_radius + 1 - kx):
-            for dy in range(-y_radius, y_radius + 1 - ky):
+        for new_x in x_range:
+            for new_y in y_range:
+                if not moore and abs(new_x - x) + abs(new_y - y) > radius:
+                    continue
 
+                neighborhood[(new_x, new_y)] = True
+
+    else:
+        # If the radius is larger than the distance from the borders, we
+        # must use a slower method, that takes into account the borders
+        # and the torus property.
+        for dx in range(-radius, radius + 1):
+            for dy in range(-radius, radius + 1):
                 if not moore and abs(dx) + abs(dy) > radius:
                     continue
 
-                nx, ny = (x + dx) % width, (y + dy) % height
-                neighborhood.append((nx, ny))
-    else:
-        x_range = range(max(0, x - radius), min(width, x + radius + 1))
-        y_range = range(max(0, y - radius), min(height, y + radius + 1))
+                new_x = x + dx
+                new_y = y + dy
 
-        for nx in x_range:
-            for ny in y_range:
+                if torus:
+                    new_x %= width
+                    new_y %= height
 
-                if not moore and abs(nx - x) + abs(ny - y) > radius:
-                    continue
-
-                neighborhood.append((nx, ny))
+                if not out_of_bounds((new_x, new_y), width, height):
+                    neighborhood[(new_x, new_y)] = True
 
     if not include_center and neighborhood:
         neighborhood.remove(pos)
